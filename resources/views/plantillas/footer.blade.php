a2e73e019e092dfba6bfa281a76f4947c199f37b
laumf <laumf@owo>
2020-02-04 09:35:30 +0100

    Creada la plantilla maestra con el contenido común a todas las vistas. Modificado el footer, que aparecerá en todas las vistas del admin. Eliminadas las plantillas header y vistasadmin.

--- resources/views/plantillas/footer.blade.php
+++ resources/views/plantillas/footer.blade.php
@@ -1,2 +1,30 @@
-<?php ?>
+        <!-- Esta es la plantilla del footer, que contiene la ventana de información. Se incluirá en todas las vistas del administrador -->
+        <!-- VENTANA MODAL INFO -->
+        <section class="modal fade" id="info">
+            <div class="modal-dialog modal-dialog-centered">
+                <div class="modal-content">
+                    <div class="modal-header bg-orange text-white px-4">
+                        <div class="modal-title">Información</div>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Cerrar">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body p-4">
+                        Creado por:
+                        <ul>
+                            <li>Laura Mª Fernández Cambronero</li>
+                            <li>Isabel de Marcos López</li>
+                            <li>Víctor Úbeda Castilla</li>
+                            <li>Carlos Morales Gallego</li>
+                        </ul>
+                        <hr>
+                        Iconos de <a href="https://fontawesome.com/license" class="text-orange">Fontawesome</a>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- FOOTER -->
+        <footer>
+            <a class="py-2 px-3 text-secondary float-right" data-toggle="modal" data-target="#info"><i class="fas fa-info-circle h2 m-0"></i></a>
+        </footer>
 
---
+++ resources/views/plantillas/maestra.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- CSS -->
+        <link rel="stylesheet" href="{{ asset('files/bootstrap-4.3.1-dist/css/bootstrap.min.css') }}">
+        <link rel="stylesheet" href="{{ asset('css/general.css') }}">
+        <title>@yield('titulo')</title>
+    </head>
+    <body>
+        <div>@yield('contenido')</div>
+        <!-- SCRIPTS -->
+        <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+        <script src="{{ asset('files/bootstrap-4.3.1-dist/js/bootstrap.jquery-3.4.1.slim.min.js') }}"></script>
+        <script src="{{ asset('files/bootstrap-4.3.1-dist/js/bootstrap.popper.min.js') }}"></script>
+        <script src="{{ asset('files/bootstrap-4.3.1-dist/js/bootstrap.min.js') }}"></script>
+        <script src="https://kit.fontawesome.com/686f2338a1.js" crossorigin="anonymous"></script>
+        <script src="{{ asset('js/ajax.js') }}"></script>
+    </body>
+</html>
