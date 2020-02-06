c5eadcbf0e8edd21235ffc8d4c874d5a8b6b0bdb
Shorgar <victorubedacastilla@gmail.com>
2020-02-06 10:31:45 +0100

    Creacion ventana modal añadir usuario y su css.

--- resources/views/vistasadmin/crudusuario.blade.php
+++ resources/views/vistasadmin/crudusuario.blade.php
@@ -26,24 +26,83 @@ Gestión
     <?php foreach ($datos as $dato) { ?>
         <form id="form" name='form' class='form'>
             @csrf
-            <input type="hidden" class="id" name="Id" id="id<?php echo $dato->Id_usuario ?>"  value="<?php echo $dato->Id_usuario ?>" >
-            <input type="text" name="Nick"  id="nick<?php echo $dato->Id_usuario ?>" value="<?php echo $dato->Nick ?>" >
-            <input type="text" name="Nombre" id="nombre<?php echo $dato->Id_usuario ?>" value="<?php echo $dato->Nombre ?>" >
+            <input type="hidden" class="id" name="Id" id="id<?php echo $dato->Id_usuario ?>"  value="<?php echo $dato->Id_usuario ?>" class="" >
+            <input type="text" name="Nick"  id="nick<?php echo $dato->Id_usuario ?>" value="<?php echo $dato->Nick ?>"  class="">
+            <input type="text" name="Nombre" id="nombre<?php echo $dato->Id_usuario ?>" value="<?php echo $dato->Nombre ?>"  class="">
             <select name="Rol" id="rol<?php echo $dato->Id_usuario ?>" class="form-control">
                 <?php foreach ($datos2 as $da2) { ?>
-                    <option name="" value="<?php echo $da2->Id_rol ?>"<?php if ($dato->Id_rol == $da2->Id_rol) { ?>selected<?php } ?>><?php echo $da2->Descripcion ?></option>
+                    <option name="" value="<?php echo $da2->Id_rol ?>"<?php if ($dato->Id_rol == $da2->Id_rol) { ?>selected<?php } ?> class=""><?php echo $da2->Descripcion ?></option>
                     <?php
                 }
                 ?>
             </select>
-            <button id="eliminar<?php echo $dato->Id_usuario ?>" class="formbutton" name="eliminar">Eliminar</button>
-            <button id='modificar<?php echo $dato->Id_usuario ?>' class="formbutton" name="modificar">Modificar</button><br/>
+            <button id="eliminar<?php echo $dato->Id_usuario ?>" name="eliminar"  class="formbutton">Eliminar</button>
+            <button id='modificar<?php echo $dato->Id_usuario ?>'  name="modificar"class="formbutton">Modificar</button><br/>
         </form>
         <?php
     }
     ?>
-    <a href="addUsuario">add</a>
+    <button data-toggle="modal" data-target="#nuevo" class="btn btn-orange">Añadir Usuario</button>
     {{ $datos->links() }}
+    <!-- VENTANA MODAL AÑADIR USUARIO -->
+    <section class="modal fade" id="nuevo">
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+                <div class="modal-header bg-orange text-white px-4">
+                    <div class="modal-title">Añadir Usuario</div>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Cerrar">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body p-4">
+                    <form action="registrar" method="post">
+                        @csrf
+                        <div class="form-group">
+                            <label class="sr-only" for="usuario">Usuario</label>
+                            <div class="input-group">
+                                <div class="input-group-prepend">
+                                    <div class="input-group-text w-8"><i class="fas fa-user icono"></i>Usuario</div>
+                                </div>
+                                <input type="text" name="usuario" id="usuario" placeholder="Usuario" class="form-control">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="sr-only" for="nombre">Nombre</label>
+                            <div class="input-group">
+                                <div class="input-group-prepend">
+                                    <div class="input-group-text w-8"><i class="fas fa-address-card icono"></i>Nombre</div>
+                                </div>
+                                <input type="text" name="nombre" id="nombre"  placeholder="Nombre" class="form-control">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="sr-only" for="clave">Contraseña</label>
+                            <div class="input-group">
+                                <div class="input-group-prepend">
+                                    <div class="input-group-text w-8"><i class="fas fa-key icono"></i>Contraseña</div>
+                                </div>
+                                <input type="password" name="clave" id="clave" placeholder="Escriba su nueva contraseña" class="form-control">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="sr-only" for="claverepe">Confirmar</label>
+                            <div class="input-group">
+                                <div class="input-group-prepend">
+                                    <div class="input-group-text w-8"><i class="fas fa-copy icono"></i>Confirmar</div>
+                                </div>
+                                <input type="password" name="claverep" id="claverepe" placeholder="Repita la contraseña" class="form-control">
+                            </div>
+                        </div>   
+                        <div class="custom-control custom-checkbox mb-3">
+                            <input id="rol" type="checkbox" name="rol" value="Admin" class="custom-control-input">
+                            <label for="rol" class="custom-control-label">¿Hacer Administrador?</label>                              
+                        </div>
+                        <input type="submit" name="validar" id="validar" value="Añadir" class="btn btn-orange w-100">  
+                    </form>
+                </div>
+            </div>
+        </div>
+    </section>
 </main>
 @include('plantillas/footer')
 @endsection
