5a2933fc89efe776209b4d008382ccfa3189f721
laumf <laumf@owo>
2020-02-06 09:40:54 +0100

    Cambiado el evento de la validación de la contraseña del perfiladmin de blur a keyup.

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -4,9 +4,10 @@
 $(document).ready(function () {
     
     //Valida el contenido de los campos de clave y repetir clave del perfil de administrador.
-    $("#claverepe").blur(function validarClave() {
+    $("#claverepe").keyup(function validarClave() {
         var mensaje = "Las contraseñas no coinciden";
-        if ($("#clave").val() !== $("#claverepe").val()) {
+        var longitud = $("#clave").val().length;
+        if ($("#clave").val() !== $("#claverepe").val() && $("#claverepe").val().length >= longitud) {
             $("#mensaje").html(mensaje);
             $("#guardar").attr('disabled', true);
         } else {
