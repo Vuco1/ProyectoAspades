4e43a8f5ef9bc68094776dfee1b277d68e3161eb
laumf <laumf@owo>
2020-02-10 22:38:35 +0100

    Contextos centrados vertical y horizontalmente en la vista.

--- resources/views/vistasusuario/contextosusuario.blade.php
+++ resources/views/vistasusuario/contextosusuario.blade.php
@@ -3,8 +3,8 @@
 Contextos
 @endsection
 @section('contenido')
-<main class="container-fluid">
-    <div id="carouselContextos" class="carousel slide" data-ride="carousel" data-interval="false" data-touch="true">
+<main class="d-flex">
+    <div id="carouselContextos" class="carousel slide m-auto" data-ride="carousel" data-interval="false" data-touch="true">
         <div class="carousel-inner">
         <?php
         $cont = 0;
@@ -15,20 +15,20 @@ Contextos
                 } else {
                     echo '<div class="carousel-item">';
                 }
-                echo '<div class="card-group">';
+                echo '<div class="card-deck">';
             } ?>
-            <form action="contextosUsuario" method="post">
-                @csrf
-                <input type="hidden" name="id" value="<?php echo $c->Id_tablero; ?>">
-                <button class="btn">
-                    <div class="card">
-                        <img src="<?php echo $c->Foto; ?>" class="card-img-top" alt="Imagen del contexto">
-                        <div class="card-body p-2">
-                            <p class="card-text"><?php echo $c->Nombre; ?></p>
-                        </div>
-                    </div>
-                </button>
-            </form>
+            <div class="card">
+                <form action="contextosUsuario" method="post">
+                    @csrf
+                    <input type="hidden" name="id" value="<?php echo $c->Id_tablero; ?>">
+                    <button class="btn">
+                            <img src="<?php echo $c->Foto; ?>" class="card-img-top" alt="Imagen del contexto">
+                            <div class="card-body p-2">
+                                <p class="card-text"><?php echo $c->Nombre; ?></p>
+                            </div>
+                    </button>
+                </form>
+            </div>
             <?php if (($cont + 1) % 3 == 0) {
                     echo '</div>'
                     . '</div>';
