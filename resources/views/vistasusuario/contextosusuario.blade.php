62cc2fb19aa86884d134a0696c9a69141c24314d
laumf <laumf@owo>
2020-02-10 14:30:14 +0100

    Convertidas las cards a button

--- resources/views/vistasusuario/contextosusuario.blade.php
+++ resources/views/vistasusuario/contextosusuario.blade.php
@@ -7,29 +7,37 @@ Contextos
     <div id="carouselContextos" class="carousel slide" data-ride="carousel" data-interval="false" data-touch="true">
         <div class="carousel-inner">
         <?php
-        $inicio = true;
-        $cont = 3; //Número de contextos por página del carrousel
-        if ($inicio == true) { ?>
+        $active = true; //Marca el primer carrousel-item
+        $cont = 0; //Número de contextos por página del carrousel
+        if ($cont % 3 == 0) {
+            if ($active == true) { ?>
             <div class="carousel-item active">
-        <?php 
-            $inicio = false;
-        } else { ?>
+            <?php 
+                $active = false;
+            } else { ?>
             <div class="carousel-item">
-        <?php } ?>
+            <?php } ?>
                 <div class="card-group">
-        <?php foreach ($contextos as $c) { ?>
+                <?php foreach ($contextos as $c) { ?>
                     <form action="contextosUsuario" method="post">
                         @csrf
-                        <div class="card">
-                            <img src="<?php echo $c->Foto; ?>" class="card-img-top" alt="Imagen del contexto">
-                            <div class="card-body">
-                                <p class="card-text"><?php echo $c->Nombre; ?></p>
+                        <input type="hidden" name="id" value="<?php echo $c->Id_tablero; ?>">
+                        <button>
+                            <div class="card">
+                                <img src="<?php echo $c->Foto; ?>" class="card-img-top" alt="Imagen del contexto">
+                                <div class="card-body">
+                                    <p class="card-text"><?php echo $c->Nombre; ?></p>
+                                </div>
                             </div>
-                        </div>
+                        </button>
                     </form>
-        <?php $cont--; } ?>
+                <?php
+                    $cont++;
+                }
+                ?>
                 </div>
             </div>
+            <?php } ?>
         </div>
     </div>
 </main>
