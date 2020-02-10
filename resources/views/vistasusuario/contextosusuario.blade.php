b0efb6ea009032221184ed926a9b74e2d10b931b
laumf <laumf@owo>
2020-02-10 13:40:45 +0100

    Creada estructura base de los contextos del usuario.

--- resources/views/vistasusuario/contextosusuario.blade.php
+++ resources/views/vistasusuario/contextosusuario.blade.php
@@ -21,74 +21,13 @@ Contextos
                     <form action="contextosUsuario" method="post">
                         @csrf
                         <div class="card">
-                            <img src="<?php echo $c->Foto; ?>" class="card-img-top" alt="Imagen del tablero">
+                            <img src="<?php echo $c->Foto; ?>" class="card-img-top" alt="Imagen del contexto">
                             <div class="card-body">
-                                <p class="card-text">Tablero 1</p>
+                                <p class="card-text"><?php echo $c->Nombre; ?></p>
                             </div>
                         </div>
                     </form>
-        <?php } ?>
-
-                
-
-
-
-
-
-                    
-                </div>
-
-                <div class="carousel-item">
-                    <div class="card-group">
-                        <div class="card">
-                            <img src="images/aryan-dhiman-iGLLtLINSkw-unsplash.jpg" class="card-img-top" alt="...">
-                            <div class="card-body">
-                                <p class="card-text">Tablero 1</p>
-                            </div>
-                        </div>
-                        <div class="card">
-                            <img src="images/gratisography-laptop-colorful-keys.jpg" class="card-img-top" alt="...">
-                            <div class="card-body">
-                                <p class="card-text">Tablero 2</p>
-                            </div>
-                        </div>
-                        <div class="card">
-                            <img src="images/img1.jpg" class="card-img-top" alt="...">
-                            <div class="card-body">
-                                <p class="card-text">Tablero 3</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="carousel-item" >
-                    <div class="card-group">
-                        <div class="card">
-                            <img src="images/img2.jpg" class="card-img-top" alt="...">
-                            <div class="card-body">
-                                <p class="card-text">Tablero 1</p>
-                            </div>
-                        </div>
-                        <div class="card">
-                            <img src="images/img3.jpg" class="card-img-top" alt="...">
-                            <div class="card-body">
-                                <p class="card-text">Tablero 2</p>
-                            </div>
-                        </div>
-                        <div class="card">
-                            <img src="images/aryan-dhiman-iGLLtLINSkw-unsplash.jpg" class="card-img-top" alt="...">
-                            <div class="card-body">
-                                <p class="card-text">Tablero 3</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-
-                <?php
-                $cont--;
-            }
-            ?>
+        <?php $cont--; } ?>
                 </div>
             </div>
         </div>
