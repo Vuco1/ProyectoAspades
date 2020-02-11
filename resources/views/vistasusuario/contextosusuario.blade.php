2b0864592d15049684c58f91ebc4c43f96e07d69
laumf <laumf@owo>
2020-02-11 09:34:38 +0100

    Añadido control y mensaje en la vista de contextosusuario en caso de que el usuario no tenga contextos.

--- resources/views/vistasusuario/contextosusuario.blade.php
+++ resources/views/vistasusuario/contextosusuario.blade.php
@@ -28,33 +28,37 @@ Contextos
     <div id="carouselContextos" class="carousel slide m-auto" data-ride="carousel" data-interval="false" data-touch="true">
         <div class="carousel-inner">
         <?php
-        $cont = 0;
-        foreach ($imgTablero as $imgT) {
-            if ($cont % 3 == 0) { //Cada 3 contextos se añade un item al carrousel
-                if ($cont == 0) {
-                    echo '<div class="carousel-item active">';
-                } else {
-                    echo '<div class="carousel-item">';
-                }
-                echo '<div class="card-deck">';
-            } ?>
-            <div class="card">
-                <form action="contextosUsuario" method="post">
-                    @csrf
-                    <input type="hidden" name="id" value="{{ $imgT->Id_imagen }}">
-                    <button class="btn">
-                            <img src="{{ $imgT->Ruta }}" class="card-img-top" alt="Imagen del contexto">
-                            <div class="card-body p-2">
-                                <p class="card-text">{{ $imgT->Nombre }}</p>
-                            </div>
-                    </button>
-                </form>
-            </div>
-            <?php if (($cont + 1) % 3 == 0) {
-                    echo '</div>'
-                    . '</div>';
-                }
-            $cont++;
+        if (!$imgTablero) { ?>
+        <p>Sin Resultados</p><?php
+        } else {
+            $cont = 0;
+            foreach ($imgTablero as $imgT) {
+                if ($cont % 3 == 0) { //Cada 3 contextos se añade un item al carrousel
+                    if ($cont == 0) {
+                        echo '<div class="carousel-item active">';
+                    } else {
+                        echo '<div class="carousel-item">';
+                    }
+                    echo '<div class="card-deck">';
+                } ?>
+                <div class="card">
+                    <form action="contextosUsuario" method="post">
+                        @csrf
+                        <input type="hidden" name="id" value="{{ $imgT->Id_imagen }}">
+                        <button class="btn">
+                                <img src="{{ $imgT->Ruta }}" class="card-img-top" alt="Imagen del contexto" class="img-perfil">
+                                <div class="card-body p-2">
+                                    <p class="card-text">{{ $imgT->Nombre }}</p>
+                                </div>
+                        </button>
+                    </form>
+                </div>
+                <?php if (($cont + 1) % 3 == 0) {
+                        echo '</div>'
+                        . '</div>';
+                    }
+                $cont++;
+            }
         } ?>
         </div>
     </div>
