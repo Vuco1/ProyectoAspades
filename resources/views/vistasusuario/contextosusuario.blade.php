d773d8d11712d7c85096a338a8d0946655e74808
laumf <laumf@owo>
2020-02-26 18:52:18 +0100

    Cambiado el mensaje que aparece en la vista de contextos cuando el usuario no tiene contextos.

--- resources/views/vistasusuario/contextosusuario.blade.php
+++ resources/views/vistasusuario/contextosusuario.blade.php
@@ -42,9 +42,10 @@ Contextos
                 ?>
             </ol>
         <?php } ?>
-        <div class="carousel-inner h-100">
+        <div class="carousel-inner h-100 text-center">
             <?php if (!$contextos) { ?>
-                <h1 class="m-auto">Sin Resultados</h1>
+                <h2 class="m-auto">Todavía no tienes ningún contexto</h2>
+                <h3 class="m-auto">Añade alguno desde la opción <span class="text-orange"><i class="fas fa-plus-circle icono"></i>Añadir contexto</span> del menú de <span class="text-orange"><i class="fas fa-lock icono"></i>Administración de tableros</span>.</h3>
             <?php } else {
                 $cont = 0;
                 foreach ($contextos as $c) {
