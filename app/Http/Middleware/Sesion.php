df647fb589332d027f2a0f5f0fa296592d737f01
carlox1998 <cmoralesgallego@gmail.com>
2020-03-02 18:07:39 +0100

    Mantiene a un usuario en la pagina de perfil al cerrar el navegador

--- app/Http/Middleware/Sesion.php
+++ app/Http/Middleware/Sesion.php
@@ -4,8 +4,8 @@ namespace App\Http\Middleware;
 
 use Closure;
 
-class Sesion
-{
+class Sesion {
+
     /**
      * Handle an incoming request.
      *
@@ -13,13 +13,16 @@ class Sesion
      * @param  \Closure  $next
      * @return mixed
      */
-    public function handle($request, Closure $next)
-    {
-        if(session()){
-            return view('vistasusuario/iniciousuario');
-        }else{
+    public function handle($request, Closure $next) {
+        if (session() != null) {
+            if (\Session::get('rol') == 1) {
+                return response()->view('vistasadmin/inicioadmin', [], 401);
+            } else {
+                return response()->view('vistasusuario/iniciousuario', [], 401);
+            }
+        } else {
             return $next($request);
         }
-        
     }
+
 }
